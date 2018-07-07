1926a2d4ffc333d9fa62f6543ec0c6abdc442674
Dylan Rutter <dylanrutter@Dylans-MBP.attlocal.net>
2018-07-07 01:52:28 -0500

    cleaning up

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,61 +1,45 @@
-// Your apiRoutes.js file should contain two routes:
+// our apiRoutes.js file should contain two routes:
 
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+var path = require("path");
 
-
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
-var friendData = require("../data/friends");
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+var userArray = require("../data/friends.js");
 
 module.exports = function (app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
-  app.get("/api/friends", function (req, res) {
-    res.json(friendData);
+  app.get('/api/friends', function (req, res) {
+    res.json(userArray);
   });
 
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
+  app.post('/api/friends', function (req, res) {
 
-  app.post("/api/friends", function (req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
-    if (friendData.length < 5) {
-      friendData.push(req.body);
-      res.json(true);
+    // Capture the user input object
+    var newUserInput = {
+      name: req.body.name,
+      photo: req.body.photo,
+      scores: []
+    };
+    console.log('new User Input name - ' + req.body.name);
+    // store newUserInput scores in the array
+    var scoresArray = [];
+    for (var i = 0; i < req.body.scores.length; i++) {
+      scoresArray.push(parseInt(req.body.scores[i]))
     }
+    newUserInput.scores = scoresArray;
+    console.log('new User Input Scores  Array = ' + scoresArray);
 
-  });
+    //  check the new User Input entry with the existing ones
+    var scoreDiffArray = [];
+    for (var i = 0; i < userArray.length; i++) {
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+      // Check each friend's scores and summarize the difference 
+      var scoreDiff = 0;
+      for (var j = 0; j < newUserInput.scores.length; j++) {
+        scoreDiff += Math.abs(newUserInput.scores[j] - userArray[i].scores[j]);
+        console.log('scoreDifference = ' + scoreDiff + "  j = " + j);
 
-  app.post("/api/clear", function () {
-    // Empty out the arrays of data
-    friendData = [];
-    // waitListData = [];
-
-    console.log(friendData);
-  });
+      }
+    }
+  })
 };
